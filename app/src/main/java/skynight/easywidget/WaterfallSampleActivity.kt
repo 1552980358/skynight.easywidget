a03d4737a22f4e6ed25b46ca6182933abf1c8b30
HuLi <1552980358@qq.com>
2019-05-01 13:50:06 +0800

    Add Sample activity for WaterfallScrollView

---
+++ app/src/main/java/skynight/easywidget/WaterfallSampleActivity.kt
@@ -0,0 +1,52 @@
+package skynight.easywidget
+
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import kotlinx.android.synthetic.main.activity_waterfallsample.*
+import skynight.easywidget.view.WaterfallScrollView
+
+
+/*
+ * @File:   WaterfallSampleActivity
+ * @Author: 1552980358
+ * @Time:   12:47 PM
+ * @Date:   1 May 2019
+ * 
+ */
+
+class WaterfallSampleActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_waterfallsample)
+        setSupportActionBar(toolbar)
+
+        Thread {
+            // init waterfall
+            val waterfallScrollView = WaterfallScrollView(this)
+
+
+            for (i: Int in 0 until 7) {
+                // add item to waterfall
+                val item = WaterfallScrollView.WaterfallCardViewItem(this)
+                item.setTitle("Title $i") // title
+                    .setSubTitle("SubTitle $i") // subtitle
+                    .setBanner(ContextCompat.getDrawable(this, R.drawable.ic_launcher_background)!!) // banner
+                    .setOnClickListener { runOnUiThread { Toast.makeText(this, "$i", LENGTH_SHORT).show() } }
+                waterfallScrollView.addView(item)
+
+                // no title & subtitle item
+                val item2 = WaterfallScrollView.WaterfallCardViewItem(this)
+                item2.hideTitle()
+                    .hideSubTitle()
+                    .setBanner(ContextCompat.getDrawable(this, R.drawable.ic_launcher_background)!!)
+                    .setOnClickListener { runOnUiThread { Toast.makeText(this, "NoTitle&SubTitle", LENGTH_SHORT).show() } }
+                waterfallScrollView.addView(item2)
+            }
+
+            runOnUiThread { nestedScrollView.addView(waterfallScrollView) } // add to ui
+        }.start()
+    }
+}
